1f14934e264fe87976cb97addf5e3d62d9d4aba0
xuxian <569642263@qq.com>
2021-06-20 09:22:59 +0800

    修改了报告完成或未完成后的日志格式

--- ReportScript.py
+++ ReportScript.py
@@ -210,12 +210,12 @@ else:
         # -----------------------晚报完成----------------------------#
 
 if driver.current_url == 'https://selfreport.shu.edu.cn/Default.aspx':
-    file_obj.write('两报完成')
+    file_obj.write('两报完成，')
 else:
-    file_obj.write('两报失败')
+    file_obj.write('两报失败，')
 
 NowTime = datetime.datetime.now()
-file_obj.write('\t' + NowTime.strftime('%Y/%m/%d %H:%M:%S'))
+file_obj.write(NowTime.strftime('%Y/%m/%d %H:%M:%S'))
 
 file_obj.close()
 driver.quit()
